cb55ba8380370bc1f5b45f1b9abdd2653cf122e2
nandha1471 <nandha.backup.ac@gmail.com>
2022-08-26 11:54:23 +0530

    message

--- src/test/java/org/git/GIT_BASIC.java
+++ src/test/java/org/git/GIT_BASIC.java
@@ -1,5 +1,17 @@
 package org.git;
 
 public class GIT_BASIC {
+	private void method1() {
+		System.out.println(1);
+	}
 
+	private void method2() {
+	System.out.println(2);
+
+	}
+
+private void method3() {
+	System.out.println(3);
 }
+}
+
